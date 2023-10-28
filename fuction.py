6d9f7784684b14e39ccffce63d0c57d32c6b5e4a
khadiza21 <bibikhadiza474@gmail.com>
2023-10-28 19:24:39 +0600

    function

---
+++ fuction.py
@@ -0,0 +1,13 @@
+# fuction or method 
+def addTwoNum(a,b):
+    return a+b 
+
+
+def subTwoNum(a,b):
+    return a-b
+
+sum = addTwoNum(1000,400)
+sub = subTwoNum(1000,300)
+
+print("The sum is ",sum)
+print("The substraction is ",sub )
