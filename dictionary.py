b5b4af95e1df6b58bf7a41397beff63a46d472b2
khadiza21 <bibikhadiza474@gmail.com>
2023-11-04 22:38:39 +0600

    dictionary  in python

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+person = {
+    "name": "Khadiza",
+    "age": 18,
+    "grades": {"Algorithm": 80, "English": 50 , "Math": 60},
+    "10": [10, 100, 1000]
+
+}
+
+print(person["10"])
+print(person["grades"]["Math"])
