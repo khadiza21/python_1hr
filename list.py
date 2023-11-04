a1a2db1e99addf462a96d17bf1bada27b490f4cd
khadiza21 <bibikhadiza474@gmail.com>
2023-11-04 12:34:51 +0600

    list in python

---
+++ list.py
@@ -0,0 +1,22 @@
+slist = ["rice", "soup","lentil","beef"]
+int_list = [0,2,3,4,4,2]
+print(slist)
+
+
+int_list = [2,3,4,1,4,2,4,5,4,2,4,3]
+
+
+print(int_list)
+print(int_list[5])
+print(int_list[-1]) #slicing or getting last index 
+print(int_list[1:5]) #here 1 index is start and 5 is the last index here showing   data of index 1 to 4
+print(int_list[:5])#showing data from first index to index 5
+print(int_list[4:])#showing data from index 4 to last index
+
+
+max_value = max(int_list)
+min_value = min(int_list)
+print("Sum of the list is ", sum(int_list))
+print("Minimum value of the list ",min_value)
+print("Maximum value of the list ", max_value)
+
